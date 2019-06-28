04b67c3e553d87a51feb8588726d7193b84e59c6
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-06-28 15:49:12 +0200

    implementato script js

---
+++ app.js
@@ -0,0 +1,12 @@
+//imposto le variabili
+var nome, cognome, colore;
+
+//imposto i prompt
+nome = prompt("Qual è il tuo nome?");
+
+cognome = prompt("Qual è il tuo cognome?");
+
+colore = prompt("Qual è il tuo colore preferito?");
+
+//scrivo sul body
+document.body.innerHTML = nome +cognome + colore + "19"
